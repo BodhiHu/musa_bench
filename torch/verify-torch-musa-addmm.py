dd5a19f7cbb9fa50c11ccd1190812e4e253b3840
Bodhi Hu <huaishun.hu@mthreads.com>
2025-03-24 21:26:50 +0800

    update test

---
+++ torch/verify-torch-musa-addmm.py
@@ -0,0 +1,52 @@
+import torch
+import torch_musa
+from torch._inductor.select_algorithm import extern_kernels
+reinterpret_tensor = torch.ops.inductor._reinterpret_tensor
+
+def test_addmm(addmm=torch.addmm, grad=False):
+  # addmm = torch.addmm
+  # addmm = extern_kernels.addmm
+
+  device = torch.device("musa")
+  grad = False
+
+  primals_2 = torch.tensor(
+    [0.2069, 0.3700],
+    device='musa:0', requires_grad=grad)
+  primals_3 = torch.tensor(
+    [[-1.4669,  0.1403],
+     [-0.7819,  0.4707]], device='musa:0')
+  primals_1 = torch.tensor(
+    [[ 0.5870,  0.4790],
+     [-0.2002,  0.3976]], device='musa:0', requires_grad=grad)
+
+  buf0 = torch.empty((2, 2), device='musa', dtype=torch.float32)
+
+  addmm(
+    primals_2,
+    primals_3,
+    primals_1,
+    alpha=1, beta=1,
+    out=buf0
+  )
+  print(">> buf0 =\n", buf0)
+
+  buf1 = torch.empty((2, 2), device='musa', dtype=torch.float32)
+  A = reinterpret_tensor(primals_2, (2, 2), (0, 1), 0)
+  B = reinterpret_tensor(primals_1, (2, 2), (1, 2), 0)
+  print(A.requires_grad)
+  print(B.requires_grad)
+  print(">> reinterpreted tensors:")
+  print(">> A:\n", A)
+  print(">> B:\n", B)
+  addmm(
+    A,#reinterpret_tensor(primals_2, (2, 2), (0, 1), 0),
+    primals_3,
+    B,#reinterpret_tensor(primals_1, (2, 2), (1, 2), 0),
+    alpha=1, beta=1,
+    out=buf0
+  )
+  print(">> buf1 =\n", buf1)
+
+
+test_addmm()
--- torch/verify-torch-musa-triton.py
+++ torch/verify-torch-musa-triton.py
@@ -6,25 +6,30 @@ import torch_musa
 class SimpleNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.fc = nn.Linear(input_size, output_size)
+        # self.fc1 = nn.Linear(input_size, hidden_size)
+        # self.relu = nn.ReLU()
+        # self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
+        x = self.fc(x)
+        # x = self.fc1(x)
+        # x = self.relu(x)
+        # x = self.fc2(x)
         return x
 
 
 device = torch.device("musa")
-model = SimpleNN(input_size=10, hidden_size=20, output_size=1).to(device)
+# model = SimpleNN(input_size=10, hidden_size=20, output_size=1).to(device)
+model = SimpleNN(input_size=2, hidden_size=2, output_size=2).to(device)
 model = torch.compile(model, backend="inductor", mode="max-autotune")
 # print(f"\n>> backend = \n{model._compiled_model.backend}\n\n")
 # print(f"\n>> codegen = \n{torch._inductor.codegen}\n\n")
 # torch._dynamo.utils.save_code_for_debugging(model)
 
-x = torch.randn(5, 10).to(device)
+# x = torch.randn(5, 10).to(device)
+x = torch.randn(2, 2).to(device)
 output = model(x)
 
-print(f"\b>> output = {output}\n")
+print(f"\n>> input  =\n{x}\n")
+print(f"\n>> output =\n{output}\n")
