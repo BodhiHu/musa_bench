4d8134f999acff1ef5625cc7b4c477a4f40ba65f
Bodhi Hu <huaishun.hu@mthreads.com>
2025-03-27 11:12:59 +0800

    update yolo bench

--- torch_yolo/yolo-perf.py
+++ torch_yolo/yolo-perf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import traceback
 import numpy as np
 import logging
 import platform
@@ -67,6 +68,12 @@ DEFAULT_DTYPES = [
 TRITON_TOGGLES = [True]
 DEFAULT_DEVICE = "cuda:0"
 # DEFAULT_DEVICE = "musa:0"
+COMPILE_MODES = [
+    "default",
+    "reduce-overhead",
+    "max-autotune",
+    "max-autotune-no-cudagraphs"
+]
 CWD = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -100,34 +107,34 @@ if args.debug:
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     torch._logging.set_logs(
-        all                 = logging.DEBUG,
-        dynamo              = logging.DEBUG,
-        aot                 = logging.DEBUG,
-        dynamic             = logging.DEBUG,
-        inductor            = logging.DEBUG,
-        distributed         = logging.DEBUG,
-        onnx                = logging.DEBUG,
+        # all                 = logging.DEBUG,
+        # dynamo              = logging.DEBUG,
+        # aot                 = logging.DEBUG,
+        # dynamic             = logging.DEBUG,
+        # inductor            = logging.DEBUG,
+        # distributed         = logging.DEBUG,
+        # onnx                = logging.DEBUG,
 
-        bytecode            = True,
-        aot_graphs          = True,
-        aot_joint_graph     = True,
-        ddp_graphs          = True,
-        graph               = True,
-        graph_code          = True,
-        graph_breaks        = True,
-        graph_sizes         = True,
-        guards              = True,
-        recompiles          = True,
-        recompiles_verbose  = True,
-        trace_source        = True,
-        trace_call          = True,
+        # bytecode            = True,
+        # aot_graphs          = True,
+        # aot_joint_graph     = True,
+        # ddp_graphs          = True,
+        # graph               = True,
+        # graph_code          = True,
+        # graph_breaks        = True,
+        # graph_sizes         = True,
+        # guards              = True,
+        # recompiles          = True,
+        # recompiles_verbose  = True,
+        # trace_source        = True,
+        # trace_call          = True,
         output_code         = True,
-        schedule            = True,
-        perf_hints          = True,
-        post_grad_graphs    = True,
-        onnx_diagnostics    = True,
-        fusion              = True,
-        overlap             = True,
+        # schedule            = True,
+        # perf_hints          = True,
+        # post_grad_graphs    = True,
+        # onnx_diagnostics    = True,
+        # fusion              = True,
+        # overlap             = True,
     )
 
 def verify_musa(choice: Literal["simple", "naive-yolo"], verify_only = False):
@@ -144,7 +151,6 @@ def verify_musa(choice: Literal["simple", "naive-yolo"], verify_only = False):
         model_path = "yolov8m.pt"
         model_data = torch.load(model_path, map_location="cuda")
         model = model_data["model"].float().eval()
-        print(f">> model = {type(model)}\n")
         model = torch.compile(model, backend="inductor", mode="max-autotune")
         input_tensor = np.load('img_0.npy')
         input_tensor = torch.from_numpy(input_tensor).to("cuda")
@@ -173,7 +179,9 @@ def benchmark(
     verbose=False,
     eps=1e-3,
     format="-",
-    triton=True
+    triton=True,
+    compile_mode="default",
+    trial_run=False
 ):
 
     model_name = model
@@ -188,16 +196,20 @@ def benchmark(
     device = select_device(device, verbose=False)
 
     def may_load_triton_model(model: YOLO):
-        # using triton musa backend
         if triton:
-            print(f"INFO: using triton backend for model {type(model.model)} on {device}")
-            # model.model.to(device)
+            if not trial_run:
+                print(f"INFO: compile model {type(model.model)} on {device}, mode = {compile_mode}")
             model.model.to(device).eval()
             model.model = torch.compile(
                 model.model,
                 backend="inductor",
-                mode="max-autotune"
+                mode=compile_mode
             )
+            # if trial_run:
+            #     input_tensor = np.load('img_0.npy')
+            #     input_tensor = torch.from_numpy(input_tensor).to("cuda")
+            #     with torch.no_grad():
+            #         model.model(input_tensor)
 
     model = YOLO(model)
     may_load_triton_model(model)
@@ -213,6 +225,7 @@ def benchmark(
     if format_arg:
         formats = frozenset(export_formats()["Argument"])
         assert format in formats, f"Expected format to be one of {formats}, but got '{format_arg}'."
+
     for i, (name, format, suffix, cpu, gpu, _) in enumerate(zip(*export_formats().values())):
         emoji, filename = "❌", None  # export defaults
         try:
@@ -234,6 +247,10 @@ def benchmark(
                 assert suffix in str(filename), "export failed"
             emoji = "❎"  # indicates export succeeded
 
+            if trial_run:
+                exported_model.predict(ASSETS / "bus.jpg", imgsz=imgsz, device=device, half=half, verbose=False)
+                return
+
             exported_model.predict(ASSETS / "bus.jpg", imgsz=imgsz, device=device, half=half, verbose=False)
 
             # Validate
@@ -244,9 +261,9 @@ def benchmark(
             fps = round(1000 / (speed + eps), 2)  # frames per second
             y.append([name, "✅", round(file_size(filename), 1), round(metric, 4), round(speed, 2), fps])
         except Exception as e:
-            if verbose:
-                assert type(e) is AssertionError, f"Benchmark failure for {name}: {e}"
-            LOGGER.warning(f"ERROR ❌️ Benchmark failure for {name}: {e}")
+            print(f"Benchmark failure for {name}: {e}")
+            traceback.print_exc()
+            LOGGER.warning(f"\nERROR ❌️ Benchmark failure for {name}: {e}")
             y.append([name, emoji, round(file_size(filename), 1), None, None, None])  # mAP, t_inference
 
     # Print results
@@ -257,7 +274,7 @@ def benchmark(
     dt = time.time() - t0
 
     row = y[0]
-    print_table_row(bf, model_name, row[2], half, batch, data, imgsz, triton, row[3], row[4], row[5])
+    print_table_row(bf, model_name, row[2], half, batch, data, imgsz, triton, compile_mode, row[3], row[4], row[5])
 
     legend = "Benchmarks legend:  - ✅ Success  - ❎ Export passed but validation failed  - ❌️ Export failed"
     s = f"\nBenchmarks complete for {name} on {data} at imgsz={imgsz} ({dt:.2f}s)\n{legend}\n{df.fillna('-')}\n"
@@ -285,8 +302,11 @@ tr:nth-child(even) { background-color: rgba(99,99,99,0.3); }
     bf.write(f"| :--------------: | :--------------: | :--------------: | :--------------: | :--------------: | :--------------: | :--------------: | :--------------: | :--------------: | :--------------: |\n")
     bf.flush()
 
-def print_table_row(bf, model, size, half, batch, dataset, imgsz, triton, metrics_mAP50, ms_per_img, fps):
-    bf.write(f"| {model:<17}| {size:<17}| {half:<17}| {batch:<17}| {dataset:<17}| {imgsz:<17}| {triton:<17}| {metrics_mAP50:<17}| {ms_per_img:<17}| {fps:<17}|\n")
+def print_table_row(bf, model, size, half, batch, dataset, imgsz, triton, compile_mode, metrics_mAP50, ms_per_img, fps):
+    if triton:
+        bf.write(f"| {model:<17}| {size:<17}| {half:<17}| {batch:<17}| {dataset:<17}| {imgsz:<17}| {(compile_mode):<17}| {metrics_mAP50:<17}| {ms_per_img:<17}| {fps:<17}|\n")
+    else:
+        bf.write(f"| {model:<17}| {size:<17}| {half:<17}| {batch:<17}| {dataset:<17}| {imgsz:<17}| {(triton):<17}| {metrics_mAP50:<17}| {ms_per_img:<17}| {fps:<17}|\n")
     bf.flush()
 
 
@@ -296,8 +316,15 @@ def main(models: List[str], batches: List[int], dtypes: List[bool], dataset: str
     with open(f"{CWD}/benchmarks/benchmarks-table-{current_ts}.md", "w", errors="ignore", encoding="utf-8") as bf:
         print_table_head(bf)
         for half, model, batch, triton, in product(dtypes, models, batches, TRITON_TOGGLES):
-            benchmark(bf=bf, model=model, data=dataset, imgsz=imgsz, batch=batch, half=half, int8=True, device=device, triton=triton)
-            time.sleep(5)
+            if triton:
+                print("\n")
+                benchmark(bf=bf, model=model, data=dataset, imgsz=imgsz, batch=batch, half=half, int8=True, device=device, triton=triton, compile_mode="default", trial_run=True)
+                for compile_mode in COMPILE_MODES:
+                    benchmark(bf=bf, model=model, data=dataset, imgsz=imgsz, batch=batch, half=half, int8=True, device=device, triton=triton, compile_mode=compile_mode)
+            else:
+                benchmark(bf=bf, model=model, data=dataset, imgsz=imgsz, batch=batch, half=half, int8=True, device=device, triton=triton)
+
+            time.sleep(1)
 
 
 if __name__ == "__main__":
